680a2490950108667ceeddc5ed7b5867e68c66b5
EDKB <edkb4101@gmail.com>
2023-07-24 22:18:58 +0300

    lesson-3 ex11

---
+++ test_homework_ex11.py
@@ -0,0 +1,10 @@
+import pytest
+import requests
+
+class TestHWex11:
+
+    def test_get_cookie(self):
+        response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
+        print(response.cookies)
+        home_work_cookie = "hw_value"
+        assert home_work_cookie in response.cookies["HomeWork"], "There is no auth cookie n the response"
